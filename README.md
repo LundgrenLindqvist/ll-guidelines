a68670a32649b9f1908b64aca4dfaa9515ffc9c3
Carl-Johan Lindqvist <cj.lindqvist@gmail.com>
2020-11-13 16:52:28 +0100

    Revisited the initial app and made some edits

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 This repo is an ongoing project to keep our coding standards up-to-date
 
 
-Changelog
+## Changelog
 
-2020-08-17 First Created
+### 2020-11-13
+
+* Fixed broken JS refs
+* Updated the panel with more options for spacing and color
+* Made blocks contenteditable
+* Added a .gitignore and removed files not needed tracking
+* Added .editorconfig
+* Updated the readme
+
+### 2020-08-17
+
+* First Created
--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 const root = document.documentElement;
 const base_input = document.getElementById("base");
-const gutter_input = document.getElementById("gutter");
 const max_width_input = document.getElementById("max_width");
+const outer_margin_input = document.getElementById("outer_margin");
+const gutter_input = document.getElementById("gutter");
+const padding_input = document.getElementById("padding");
+const color_base_input = document.getElementById("color_base");
+const color_contrast_input = document.getElementById("color_contrast");
+const color_accent_input = document.getElementById("color_accent");
 
 base_input.addEventListener("change", e => {
     root.style.setProperty('--base_size', base_input.value+"px");
 });
 
-gutter_input.addEventListener("change", e => {
-    root.style.setProperty('--gutter', base_input.value+"em");
+max_width_input.addEventListener("change", e => {
+    root.style.setProperty('--max_width', max_width_input.value+"px");
 });
 
-max_width_input.addEventListener("change", e => {
-    root.style.setProperty('--max_width', base_input.value+"px");
+outer_margin_input.addEventListener("change", e => {
+    root.style.setProperty('--outer_margin', outer_margin_input.value+"em");
+});
+
+gutter_input.addEventListener("change", e => {
+    root.style.setProperty('--gutter', gutter_input.value+"em");
+});
+
+padding_input.addEventListener("change", e => {
+    root.style.setProperty('--padding', padding_input.value+"em");
+});
+
+color_base.addEventListener("change", e => {
+    root.style.setProperty('--color_base', color_base.value);
+});
+
+color_contrast.addEventListener("change", e => {
+    root.style.setProperty('--color_contrast', color_contrast.value);
+});
+
+color_accent.addEventListener("change", e => {
+    root.style.setProperty('--color_accent', color_accent.value);
 });
